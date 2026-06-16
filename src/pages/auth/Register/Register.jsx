c98d29f247188dca4df07acf4e4e59ea271231ff
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-16 19:47:53 +0530

    Create Register.jsx

---
+++ src/pages/auth/Register/Register.jsx
@@ -0,0 +1,17 @@
+function Register() {
+  return (
+    <div>
+
+      <h2>
+        Create Account
+      </h2>
+
+      <p>
+        Start building with the platform
+      </p>
+
+    </div>
+  );
+}
+
+export default Register;

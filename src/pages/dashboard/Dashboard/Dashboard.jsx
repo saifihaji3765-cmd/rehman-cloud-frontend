8df1a1a5d258f8769f732f3d56f9aea30aba1e5d
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-16 19:59:33 +0530

    Create Dashboard.jsx

---
+++ src/pages/dashboard/Dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+function Dashboard() {
+  return (
+    <div>
+
+      <h1>
+        Dashboard
+      </h1>
+
+      <p>
+        Welcome to your platform dashboard
+      </p>
+
+    </div>
+  );
+}
+
+export default Dashboard;

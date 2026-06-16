74a721b1cda758d1f033a17b4735c45b64cfad80
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-16 19:41:30 +0530

    Create Login.jsx

---
+++ src/pages/auth/Login/Login.jsx
@@ -0,0 +1,17 @@
+function Login() {
+  return (
+    <div>
+
+      <h2>
+        Login
+      </h2>
+
+      <p>
+        Sign in to continue
+      </p>
+
+    </div>
+  );
+}
+
+export default Login;

1a247fa9d436f466b5d2cc6bd85921788760a450
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-17 10:26:31 +0530

    Create Workspace.jsx

---
+++ src/pages/workspace/Workspace/Workspace.jsx
@@ -0,0 +1,23 @@
+import DashboardLayout from "../../../layouts/DashboardLayout/DashboardLayout.jsx";
+
+function Workspace() {
+  return (
+    <DashboardLayout>
+
+      <div>
+
+        <h1>
+          AI Workspace
+        </h1>
+
+        <p>
+          Build applications, agents, APIs and complete systems using AI.
+        </p>
+
+      </div>
+
+    </DashboardLayout>
+  );
+}
+
+export default Workspace;

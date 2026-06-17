47366deb2b71d2419ee8e62a74018c24c5e26d66
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-17 10:31:09 +0530

    Create Billing.jsx

---
+++ src/pages/billing/Billing/Billing.jsx
@@ -0,0 +1,23 @@
+import DashboardLayout from "../../../layouts/DashboardLayout/DashboardLayout.jsx";
+
+function Billing() {
+  return (
+    <DashboardLayout>
+
+      <div>
+
+        <h1>
+          Billing
+        </h1>
+
+        <p>
+          Manage subscriptions, invoices and payment methods.
+        </p>
+
+      </div>
+
+    </DashboardLayout>
+  );
+}
+
+export default Billing;

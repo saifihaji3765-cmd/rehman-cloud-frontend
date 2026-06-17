eb29484939016a66627a7f568ea1eb9c98dbaa35
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-17 10:29:47 +0530

    Create Deployments component with layout and content

---
+++ src/pages/deployments/Deployments/Deployments.jsx
@@ -0,0 +1,23 @@
+import DashboardLayout from "../../../layouts/DashboardLayout/DashboardLayout.jsx";
+
+function Deployments() {
+  return (
+    <DashboardLayout>
+
+      <div>
+
+        <h1>
+          Deployments
+        </h1>
+
+        <p>
+          Manage builds, deployments, environments and infrastructure.
+        </p>
+
+      </div>
+
+    </DashboardLayout>
+  );
+}
+
+export default Deployments;

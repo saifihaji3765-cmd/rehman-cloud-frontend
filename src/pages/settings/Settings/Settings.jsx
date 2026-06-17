978dfa3133bce58439975de2bd79113d05eac7f2
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-17 10:32:08 +0530

    Create Settings component with layout and content

---
+++ src/pages/settings/Settings/Settings.jsx
@@ -0,0 +1,23 @@
+import DashboardLayout from "../../../layouts/DashboardLayout/DashboardLayout.jsx";
+
+function Settings() {
+  return (
+    <DashboardLayout>
+
+      <div>
+
+        <h1>
+          Settings
+        </h1>
+
+        <p>
+          Manage account, security, team members and platform preferences.
+        </p>
+
+      </div>
+
+    </DashboardLayout>
+  );
+}
+
+export default Settings;

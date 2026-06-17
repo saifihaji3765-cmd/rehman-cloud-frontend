508314c5aeb32cf11d69ebae02b1298a45cd0621
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-17 12:56:04 +0530

    Create Topbar.jsx

---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+function Topbar() {
+  return (
+    <header>
+
+      <div>
+
+        <h2>
+          Enterprise Console
+        </h2>
+
+      </div>
+
+      <div>
+
+        <button>
+          Notifications
+        </button>
+
+        <button>
+          Profile
+        </button>
+
+      </div>
+
+    </header>
+  );
+}
+
+export default Topbar;

c4dc6ba993e77ef1e70390a6c1702de1d40705ab
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-18 11:49:10 +0530

    Create navigation.js

---
+++ src/config/navigation.js
@@ -0,0 +1,30 @@
+const navigation = [
+
+  {
+    name:"Dashboard",
+    path:"/dashboard"
+  },
+
+  {
+    name:"Workspace",
+    path:"/workspace"
+  },
+
+  {
+    name:"Deployments",
+    path:"/deployments"
+  },
+
+  {
+    name:"Billing",
+    path:"/billing"
+  },
+
+  {
+    name:"Settings",
+    path:"/settings"
+  }
+
+];
+
+export default navigation;

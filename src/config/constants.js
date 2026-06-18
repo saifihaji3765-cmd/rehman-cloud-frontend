7b71f16eeadae410917895e76810a6f8536a52dc
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-18 11:48:29 +0530

    Create constants.js

---
+++ src/config/constants.js
@@ -0,0 +1,11 @@
+export const APP_NAME =
+"ZyrionOS";
+
+export const APP_TAGLINE =
+"AI Cloud Operating System";
+
+export const SUPPORT_EMAIL =
+"support@zyrionos.com";
+
+export const WEBSITE_URL =
+"https://zyrionos.com";

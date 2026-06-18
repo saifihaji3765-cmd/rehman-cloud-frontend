faecc5f8bd09a5dcbab64d69490e1511f531104e
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-18 10:50:04 +0530

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,6 +6,22 @@ function Sidebar() {
   return (
     <aside className={styles.sidebar}>
 
+      {/* BRAND */}
+
+      <div className={styles.brand}>
+
+        <div className={styles.logo}>
+          ZyrionOS
+        </div>
+
+        <div className={styles.tagline}>
+          AI Cloud Operating System
+        </div>
+
+      </div>
+
+      {/* MENU */}
+
       <div className={styles.menu}>
 
         <NavLink
@@ -62,6 +78,25 @@ function Sidebar() {
         >
           Settings
         </NavLink>
+
+      </div>
+
+      {/* FOOTER */}
+
+      <div className={styles.footer}>
+
+        <div className={styles.planCard}>
+
+          <div className={styles.planTitle}>
+            Subscription
+          </div>
+
+          <div className={styles.planText}>
+            Plan information will be
+            loaded from backend.
+          </div>
+
+        </div>
 
       </div>
 

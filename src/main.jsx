5d3ec7cf442a60442c83afc0c13a467b3c84b9b8
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-06-19 09:10:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,24 @@ import App from "./App.jsx";
 
 import "./index.css";
 
+import {
+
+  AuthProvider
+
+} from "./context/AuthContext.jsx";
+
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
+
   <React.StrictMode>
-    <App />
+
+    <AuthProvider>
+
+      <App />
+
+    </AuthProvider>
+
   </React.StrictMode>
+
 );
